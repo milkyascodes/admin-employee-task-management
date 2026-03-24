9280653ea7953a63da8d7f3b41185a21fa6fa66b
miki <milkyas.dev@gmail.com>
2026-03-24 01:35:01 -0300

    created user-login functionality

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,5 +1,26 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import supabase from "../../supabaseClient";
+
+// functions
+export const loginUser = createAsyncThunk(
+  "auth/loginUser",
+  async ({ email, password }, thunkAPI) => {
+    const { data, error } = supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) return thunkAPI.rejectWithValue(error.message);
+
+    const { data: profile } = await supabase
+      .from("profiles")
+      .select("role")
+      .eq("id", data.user.id)
+      .single();
+
+    return { user: data.user, role: profile.role };
+  },
+);
+
 export const logoutUser = createAsyncThunk("auth/logoutUser", async () => {
   await supabase.auth.signOut();
 });
@@ -14,9 +35,24 @@ const authSlice = createSlice({
   },
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(logoutUser.fulfilled, (state) => {
-      ((state.user = null), (state.role = null));
-    });
+    builder
+      .addCase(loginUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload.user;
+        state.role = action.payload.role;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(loginUser.rejected, (state) => {})
+      .addCase(logoutUser.fulfilled, (state) => {
+        ((state.user = null), (state.role = null));
+      });
   },
 });
 
