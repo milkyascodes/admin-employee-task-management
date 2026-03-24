a772c1016d71ae2c7979a6b68f1707d2f10c238f
miki <milkyas.dev@gmail.com>
2026-03-24 01:19:39 -0300

    created authSlice

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: null,
+    role: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {},
+});
+
+export const authReducer = authSlice.reducer;
