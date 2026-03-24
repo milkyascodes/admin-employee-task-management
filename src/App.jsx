99b8551eae2cd849104ab84673e83372b4c0a785
miki <milkyas.dev@gmail.com>
2026-03-24 01:53:40 -0300

    implemented Login and created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/LoginPage";
+import Signup from "./pages/SignupPage";
+import UserDashboard from "./pages/UserPage";
+import AdminDashboard from "./pages/AdminPage";
 
 function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={<UserDashboard />} />
+        <Route path="/signup" element={<AdminDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,54 +1,55 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "../features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
+  const { user, role, error } = useSelector((state) => state.auth);
+
+  // login
   const handleLogin = async (e) => {
     e.preventDefault();
     console.log({ email, password });
-    // connect to Supabase login
-    await supabase.auth.signInWithPassword({
-      email,
-      password,
-    });
+    const result = await dispatch(loginUser({ email, password }));
+    if (result.meta.requestStatus === "fulfilled") {
+      if (role === "admin") navigate("/admin");
+      else navigate("/employee");
+    }
   };
 
   return (
-    <div className="flex  items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <form
-        onSubmit={handleLogin}
         className="bg-white p-8 rounded-2xl shadow-md w-full max-w-md"
+        onSubmit={handleLogin}
       >
         <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-
+        {error && <p className="text-red-500 mb-4">{error}</p>}
         <input
           type="email"
           placeholder="Email"
-          className="w-full mb-4 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+          className="w-full mb-4 px-4 py-2 border rounded-lg"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-
         <input
           type="password"
           placeholder="Password"
-          className="w-full mb-6 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+          className="w-full mb-6 px-4 py-2 border rounded-lg"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-
         <button
           type="submit"
-          className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition"
+          className="w-full bg-blue-500 text-white py-2 rounded-lg"
         >
           Login
         </button>
-
-        <p className="text-sm text-center mt-4">
-          Don’t have an account?{" "}
-          <span className="text-blue-500 cursor-pointer">Sign up</span>
-        </p>
       </form>
     </div>
   );
